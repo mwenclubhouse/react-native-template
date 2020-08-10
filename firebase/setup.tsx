63252148b0e804cd886d437dc442aa7b3386b1f8
matthewen <mattwen2018@gmail.com>
2020-08-10 15:58:43 -0500

    google services

--- firebase/setup.tsx
+++ firebase/setup.tsx
@@ -1,16 +1,16 @@
-import * as firebase from 'firebase';
-import "firebase/auth";
-
-// Initialize Firebase
-const firebaseConfig = {
-    apiKey: JSON.stringify(process.env.PURDUE_API_KEY),
-    // authDomain: ,
-    databaseURL: "https://purdue-contact-tracing.firebaseio.com/",
-    projectId: "purdue-contact-tracing",
-    // storageBucket: "project-id.appspot.com",
-    // messagingSenderId: "sender-id",
-    // appId: "app-id",
-    // measurementId: "G-measurement-id"
-};
-
-firebase.initializeApp(firebaseConfig);
+// import * as firebase from 'firebase';
+// import "firebase/auth";
+//
+// // Initialize Firebase
+// const firebaseConfig = {
+//     apiKey: JSON.stringify(process.env.PURDUE_API_KEY),
+//     // authDomain: ,
+//     databaseURL: "https://purdue-contact-tracing.firebaseio.com/",
+//     projectId: "purdue-contact-tracing",
+//     // storageBucket: "project-id.appspot.com",
+//     // messagingSenderId: "sender-id",
+//     // appId: "app-id",
+//     // measurementId: "G-measurement-id"
+// };
+//
+// firebase.initializeApp(firebaseConfig);
